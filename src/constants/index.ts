67be8d50342e3fec3beb329cf659bcacbff4d49f
Celopad Dev <support@celopad.io>
2021-10-10 18:16:23 +0700

    custom router

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 import { injected, ledger, valora } from '../connectors'
 import { UBE } from './tokens'
 
-export const ROUTER_ADDRESS = '0x1dAC7e86903Fd55483C3D05A31B4A3e92DBBb80E'
+export const ROUTER_ADDRESS = '0xE3D8bd6Aed4F159bc8000a9cD47CffDb95F96121'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
